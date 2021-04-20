8a0c6d13fde6bc16a55063d36b0e54697489bc69
hannesgregor <hannes-gregor.galkin@khk.ee>
2021-04-20 16:21:50 +0300

    logRocket

---
+++ svelte-auth/src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script>
+    import {onMount} from 'svelte';
+    import {authenticated} from '../stores/auth';
+    let message = ''
+    import LogRocket from 'logrocket';
+    LogRocket.init('sxjdxr/logrocketproject-oeitq');
+    
+
+
+    onMount(async () => {
+        
+        try {
+            const response = await fetch('http://localhost:8000/api/user', {
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include',
+            });
+
+            const content = await response.json();
+
+            message = `Hi ${content.name}`;
+            authenticated.set(true);
+            
+            
+        } catch (error) {
+            message = `You are not logged in `;
+            authenticated.set(false);
+            }
+
+});
+</script>
+
+{message}
+
